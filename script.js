b0e17ab6c447c294662ed0da294d7f6f67b5ed2e
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-20 16:15:00 +0100

    suppongo di ave finito

---
+++ script.js
@@ -0,0 +1,26 @@
+// forma con dichiarazione e assegnazione separate
+
+const client_name = prompt("Inserisci il tuo nome");
+console.log(client_name);
+console.log(typeof "client_name");
+
+const client_surname = prompt("Inserisci il tuo cognome");
+console.log(client_surname);
+console.log(typeof "client_surname");
+
+let favourite_color = prompt("Il tuo colore preferito?");
+console.log(favourite_color);
+console.log(typeof "favourite_color");
+
+let client_age = prompt("Quanti anni hai?");
+console.log(client_age);
+console.log(typeof "client_age");
+
+// forma con dichiarazione e assegnazione condensate
+
+let welcome_msg = `Benvenuto/a ${client_name} ${client_surname} ${favourite_color} ${client_age}`;
+
+console.log(welcome_msg);
+console.log(typeof welcome_msg);
+
+document.getElementById("welcome").innerHTML = welcome_msg
